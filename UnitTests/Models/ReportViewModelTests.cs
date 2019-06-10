9e9e1522893c8aff5a238bb0212d560ba2b7f006
Onur Bakiner <onur.bakiner@gmail.com>
2019-06-09 21:13:20 -0700

    added ReportViewModel Number of Users Tests

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using HW1c.Models;
 
 namespace UnitTests.Models
 {
     [TestClass]
     public class ReportViewModelTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void ReportViewModel_Instantiate_Default_Should_Pass()
         {
+            // Arrange
+
+            // Act
+            var result = new LogViewModel();
+
+            // Assert 
+            Assert.IsNotNull(result);
         }
+
+        public void ReportViewModel_NumberOfUsers_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+            // Act
+            var result = myTest.NumberOfUsers;
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
+        public void ReportViewModel_NumberOfUsers_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+            int number = 55;
+            
+            // Act
+            myTest.NumberOfUsers = number;
+            //var result = myTest.LogList;
+
+            // Assert 
+            Assert.AreEqual(55, myTest.NumberOfUsers);
+        }
+
     }
 }
+
+
